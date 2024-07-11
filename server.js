53b71fecc7d5dda4d161adfb5c4cd8ef96f86f16
Evan Wiley <edubme@gmail.com>
2024-07-10 23:04:38 -0400

    adding file changes.

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000; // Change the port to avoid conflict with the React development server
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build'))); // Serve React build files
 const { ObjectId } = require('mongodb');  // Import ObjectId from MongoDB driver if needed
 
 // Function to sanitize input
