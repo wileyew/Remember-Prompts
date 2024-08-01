ac08ba867c40d5c81773232eb0de7fa5a631b8b1
Evan Wiley <edubme@gmail.com>
2024-08-01 11:11:41 -0400

    adding amplify backend functionality for insert prompts.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -20,6 +20,13 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
+    },
+    "reportingprompts": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => (
   <footer className="bg-gray-100 dark:bg-gray-800 p-3 text-center">
     <div className="mb-3 app-logo" />
-    <p className="text-gray-900 dark:text-black-100">
+    <strong className="text-gray-900 dark:text-black-100">
       Have questions? Contact us at overflowprompts@gmail.com
-    </p>
+    </strong>
   </footer>
 );
 
