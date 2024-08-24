510283ef03f74940458c9c6076f6eaa3698ae43d
Evan Wiley <edubme@gmail.com>
2024-08-23 22:35:08 -0400

    fixing body of html

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,26 @@
 import React from "react";
-import logo from "../assets/rememberprompts.svg";
 import overflowBlackLogo from "../assets/Black logo - no background.svg";
 import overflowlogo from "../assets/Color logo - no background.svg";
-import darkmodelogo from "../assets/rememberprompts_dark_mode.svg";
 import Video from "./DriveVideoEmbed";
 
 // Assuming `mode` is passed as a prop to the Hero component
 const Hero = ({ mode }) => (
   <div className="text-center hero my-5">
-     {mode === 'dark' ? (
-      <img className="mb-3 app-logo" src={overflowBlackLogo} alt="React logo" width="120" />
+    {mode === 'dark' ? (
+      <img className="mb-3 app-logo" src={overflowBlackLogo} alt="Overflow Prompts logo" width="120" />
     ) : (
-      <img className="mb-3 app-logo" src={overflowlogo} alt="React logo" width="120" />
+      <img className="mb-3 app-logo" src={overflowlogo} alt="Overflow Prompts logo" width="120" />
     )}
-    <h1 className="mb-4">Overflow Prompts </h1>
+    <h1 className="mb-4">Overflow Prompts</h1>
     <p className="lead">
-      Community Driven AI Accountability, watch an overview about Overflow Prompts, below, and register or login to get started!
+      Community Driven AI Accountability. Watch an overview about Overflow Prompts below, and register or login to get started!
     </p>
-    <Video videoId="1AaVt0ucVoPe3UateyK2l23NjVcg2Lg8B"/>
+    <p className="lead">
+      <strong>Why participate?</strong> By reporting, upvoting, and commenting on prompts, you play a key role in improving AI for everyone. 
+      Reporting prompts helps uncover issues and drive improvements directly to AI companies AND most importantly gives visibility to AI companies to fix the issues (if deemed an issue from their perspective) you report! Upvoting highlights the most critical issues, ensuring they get the attention they deserve. 
+      Commenting allows you to share insights, suggest solutions, and collaborate with a community of like-minded individuals. Plus, as an active contributor, you'll gain early access to new features and tools, and earn recognition within the community!
+    </p>
+    <Video videoId="1AaVt0ucVoPe3UateyK2l23NjVcg2Lg8B" />
   </div>
 );
 
