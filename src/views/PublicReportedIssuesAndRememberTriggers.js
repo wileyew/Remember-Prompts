58b4ad1fdc3714408b027d6c9f885158b3e3240f
Evan Wiley <edubme@gmail.com>
2024-10-17 14:22:16 -0400

    removing auth from view file.

--- src/views/PublicReportedIssuesAndRememberTriggers.js
+++ src/views/PublicReportedIssuesAndRememberTriggers.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container } from "reactstrap";
 
 import BotpressTable from "../components/BotpressTable";
 import Loading from "../components/Loading";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 export const TableComponent = () => {
   // const { user } = useAuth0();
 
   return (
     <Container className="mb-5">
-      <BotpressTable></BotpressTable>
+      <BotpressTable />
     </Container>
   );
 };
 
-export default (TableComponent, {
-  onRedirecting: () => <Loading />,
-});
+export default TableComponent;
