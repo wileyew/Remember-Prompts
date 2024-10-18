fc5543a95f191df756b1f682c49486489aafcfde
Evan Wiley <edubme@gmail.com>
2024-10-18 13:26:11 -0400

    removing dot env from js file.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,3 @@
-require('dotenv').config(); // This line is needed to load the environment variables from the .env file
-
 const axios = require('axios'); // Assuming Axios is used for HTTP requests
 
 const data = JSON.stringify({
