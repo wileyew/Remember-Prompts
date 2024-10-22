20fbe6c6a96ec8e6e94ac11a9eda658c67c1f8d0
Evan Wiley <edubme@gmail.com>
2024-10-22 10:19:59 -0400

    removing auth0 reference along with registration and sign in.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,10 +13,10 @@ const Hero = ({ mode }) => (
     )}
     <h1 className="mb-4">Overflow Prompts</h1>
     <p className="lead">
-      Community Driven AI Accountability. Register or login using a google account to get started! Authentication for this application is powered by <a href="https://auth0.com/resources/webinars/demo-consumer-facing-applications?utm_source=google&utm_campaign=amer_namer_usa_all_ciam-all_dg-ao_auth0_search_google_text_kw_DSA_utm2&utm_medium=cpc&utm_id=aNK4z000000UCSHGA4&gad_source=1&gclid=CjwKCAjw5qC2BhB8EiwAvqa41jv5p2UHsbIBqX-PyXJLNzV6RM7yYzw_ztnn2EX8HXhtYy4jWFDNKxoCY8wQAvD_BwE">Auth0.</a>.
+      Community Driven AI Accountability
     </p>
     <p className="lead">
-      <strong>Why participate?</strong> <br></br>By reporting, upvoting, and commenting on prompts, you play a key role in improving AI for everyone. 
+      <strong>Why participate?</strong> <br></br>By reporting, up voting, and commenting on prompts, you play a key role in improving AI for everyone. 
       Reporting prompts helps uncover issues and drive improvements directly to AI companies AND most importantly gives visibility to AI companies to fix the issues (if deemed an issue from their perspective) you report! Upvoting highlights the most critical issues, ensuring they get the attention they deserve. 
       Commenting allows you to share insights, suggest solutions, and collaborate with a community of like-minded individuals. Plus, as an active contributor, you'll gain early access to new features and tools, and earn recognition within the community!
     </p>
