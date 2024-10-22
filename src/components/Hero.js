f8e3961150929c186bfb02e7aaf99aaa048e43b2
Evan Wiley <edubme@gmail.com>
2024-10-22 11:10:01 -0400

    adding typewriter effect for website.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import overflowBlackLogo from "../assets/Black logo - no background.svg";
 import overflowlogo from "../assets/Color logo - no background.svg";
-import Video from "./DriveVideoEmbed"; // Ensure this file exists and is correctly named
+import Video from "./DriveVideoEmbed";
 
+// Assuming `mode` is passed as a prop to the Hero component
 const Hero = ({ mode }) => (
   <div className="text-center hero my-5">
     {mode === 'dark' ? (
@@ -10,13 +12,26 @@ const Hero = ({ mode }) => (
     ) : (
       <img className="mb-3 app-logo" src={overflowlogo} alt="Overflow Prompts logo" width="120" />
     )}
-    <h1 className="mb-4">Overflow Prompts</h1>
+    
+    <h1 className="mb-4">
+      <Typewriter
+        options={{
+          strings: ["Educate", "Identify", "Report Issues"],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+        }}
+      />
+    </h1>
+    
     <p className="lead">
-      Community driven AI empowerment.
+      Community Driven AI Accountability
       Check out our video below to learn more about Overflow Prompts and how you can help improve AI for everyone!
     </p>
+    
     <div className="video-container">
     <iframe src="https://drive.google.com/file/d/1f79xMu5kYvZG4X-jjS918pWg1r1Cy_Kd/preview" width="640" height="480" allow="autoplay"></iframe>    </div>
+    
     <p className="lead">
       <strong>Why participate?</strong> <br></br>By reporting, up voting, and commenting on prompts, you play a key role in improving AI for everyone. 
       Reporting prompts helps uncover issues and drive improvements directly to AI companies AND most importantly gives visibility to AI companies to fix the issues (if deemed an issue from their perspective) you report! Upvoting highlights the most critical issues, ensuring they get the attention they deserve. 
