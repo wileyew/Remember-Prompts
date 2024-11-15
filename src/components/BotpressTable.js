de5800852bc4c8ee77db912a9a0d260f7cb7f1ce
Evan Wiley <edubme@gmail.com>
2024-11-15 15:15:19 -0500

    fixed method to allow POST.

--- src/components/BotpressTable.js
+++ src/components/BotpressTable.js
@@ -147,7 +147,7 @@ const BotpressTable = () => {
     
             try {
                 await fetch(`https://n7mam9mzqb.execute-api.us-east-1.amazonaws.com/Upvotes/${id}`, {
-                    method: 'PUT',
+                    method: 'POST',
                     headers: {
                         'x-api-key': 'klQ2fYOVVCMWHMAb8nLu9mR9H14gBidPOH5FbM70',
                         'Content-Type': 'application/json',
