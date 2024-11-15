adabb2b6914690525ec9301577df04f00399afe0
Evan Wiley <edubme@gmail.com>
2024-11-15 12:54:52 -0500

    giving * for CORS.

--- src/components/BotpressTable.js
+++ src/components/BotpressTable.js
@@ -151,7 +151,7 @@ const BotpressTable = () => {
                     headers: {
                         'x-api-key': 'klQ2fYOVVCMWHMAb8nLu9mR9H14gBidPOH5FbM70',
                         'Content-Type': 'application/json',
-                        'no-cors': true,
+                        'Allow-Control-Allow-Origin': '*',
                     },
                     body: JSON.stringify({ id }),
                 });
