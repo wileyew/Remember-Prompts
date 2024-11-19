8d52cd24507f80a5dbb2dd3ed803142c3560b566
Evan Wiley <edubme@gmail.com>
2024-11-19 00:09:23 -0500

    headers for api url with CORS enabled.

--- src/components/BotpressTable.js
+++ src/components/BotpressTable.js
@@ -169,7 +169,7 @@ const BotpressTable = () => {
         };
     
         try {
-            const response = await fetch(`https://6tgwnaw945.execute-api.us-east-1.amazonaws.com/comments/pets/comments/${id}`, {
+            const response = await fetch(`https://hj1k4jj2ei.execute-api.us-east-1.amazonaws.com/insert-comments/comment/${id}`, {
                 method: 'POST',
                 headers: {
                     'x-api-key': 'klQ2fYOVVCMWHMAb8nLu9mR9H14gBidPOH5FbM70',  // Same API Key as in handleUpvote
